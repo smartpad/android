245cd594d3c33ecc8fed76c5543b0d8c7c00026f
huybanh <huy.banh@jinnova.com>
2014-03-12 14:23:43 +0700

    corrected FeedManager.getFeedCount()

--- src/com/vn/foodapp/data/FeedManager.java
+++ src/com/vn/foodapp/data/FeedManager.java
@@ -1,15 +1,10 @@
 package com.vn.foodapp.data;
 
-import com.vn.foodapp.R;
 import com.vn.foodapp.dummyserver.AppServer;
 
 public class FeedManager {
 	
 	public static FeedManager instance;
-	
-	private int[] images = {R.drawable.anh1, R.drawable.anh2, R.drawable.anh3, R.drawable.anh4
-			, R.drawable.anh5, R.drawable.anh6, R.drawable.anh7, R.drawable.anh8
-			, R.drawable.anh9, R.drawable.anh10};
 	
 	private Feed[] feeds;
 	
@@ -18,7 +13,10 @@ public class FeedManager {
 	}
 
 	public int getFeedCount() {
-		return images.length;
+		if (feeds == null) {
+			feeds = AppServer.getFeeds(0, 50);
+		}
+		return feeds.length;
 	}
 	
 	public Feed getFeed(int pos) {
