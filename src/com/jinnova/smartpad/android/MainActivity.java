9716461e297878ed6fdd3bc4bfc8d9388ba77ea2
Huy Banh <huy.banh@jinnova.com>
2014-03-14 00:31:01 +0700

    package renamed.

---
+++ src/com/jinnova/smartpad/android/MainActivity.java
@@ -0,0 +1,25 @@
+package com.jinnova.smartpad.android;
+
+import com.jinnova.smartpad.android.feed.FeedManager;
+import com.jinnova.smartpad.android.viewmap.ViewMapper;
+import com.vn.foodapp.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	private ListView list;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		FeedManager.initialize();
+		setContentView(R.layout.activity_main);
+		list = (ListView) findViewById(R.id.list);
+		ViewMapper viewMapper = new ViewMapper(this);
+		list.setAdapter(new RowsAdapter(viewMapper));
+	}
+
+}
