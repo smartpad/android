eaf18c766c150c450b3165f94324a88a6e748033
vinhnguyenhoan <vinhnguyenhoan@gmail.com>
2014-03-18 22:33:07 +0700

    Untrack R.java

--- gen/com/jinnova/smartpad/R.java
+++ gen/com/jinnova/smartpad/R.java
@@ -50,18 +50,27 @@ public final class R {
         public static final int anh8=0x7f020008;
         public static final int anh9=0x7f020009;
         public static final int ic_launcher=0x7f02000a;
+        public static final int lotteria_logo=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int date=0x7f090001;
-        public static final int decription=0x7f090004;
+        public static final int action_settings=0x7f09000b;
+        public static final int date=0x7f090007;
+        public static final int dateFeed=0x7f090002;
+        public static final int decription=0x7f09000a;
+        public static final int decriptionFeed=0x7f090005;
+        public static final int goBtnFeed=0x7f090006;
+        public static final int imageBranch=0x7f090001;
+        public static final int imageFeed=0x7f090003;
         public static final int list=0x7f090000;
-        public static final int newsImage=0x7f090002;
-        public static final int title=0x7f090003;
+        public static final int newsImage=0x7f090008;
+        public static final int title=0x7f090009;
+        public static final int titleFeed=0x7f090004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int news_row=0x7f030001;
+        public static final int branch_row=0x7f030001;
+        public static final int feed_row=0x7f030002;
+        public static final int news_row=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -69,6 +78,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int go_btn=0x7f050003;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
