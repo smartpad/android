bb54a14fb6f683738d9992658e34039f78b31d92
Danh Vo <vntdanh1990@gmail.com>
2014-03-22 10:52:00 +0700

    fix feed activity

--- src/com/jinnova/smartpad/android/feed/FeedActivity.java
+++ src/com/jinnova/smartpad/android/feed/FeedActivity.java
@@ -1,7 +1,16 @@
 package com.jinnova.smartpad.android.feed;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
 
-/*public class FeedActivity extends Activity {
+import com.jinnova.smartpad.R;
+import com.jinnova.smartpad.android.localstore.StoreManager;
+
+public class FeedActivity extends Activity {
 
 	private ListView list;
 
@@ -17,4 +26,27 @@ package com.jinnova.smartpad.android.feed;
 		list = (ListView) findViewById(R.id.list);
 		list.setAdapter(new FeedViewAdapter());
 	}
-}*/
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        /*case R.id.action_search:
+	            //openSearch();
+	            return true;*/
+	        case R.id.action_settings:
+	            //openSettings();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+}
