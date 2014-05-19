ed488b877c7de70a6c98f93a3b0381ca1b22cc11
huybanh <huy.banh@jinnova.com>
2014-05-19 10:59:23 +0700

    merged #26

--- src/com/jinnova/smartpad/android/cat/BranchViewBuilder.java
+++ src/com/jinnova/smartpad/android/cat/BranchViewBuilder.java
@@ -2,11 +2,14 @@ package com.jinnova.smartpad.android.cat;
 
 import static com.jinnova.smartpad.android.ServerConstants.*;
 
+import android.graphics.drawable.Drawable;
 import android.text.method.LinkMovementMethod;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jinnova.smartpad.android.ImageLoaderTask;
 import com.jinnova.smartpad.android.R;
 import com.jinnova.smartpad.android.SmartpadViewAdapter;
 import com.jinnova.smartpad.android.UIData;
@@ -17,6 +20,7 @@ public class BranchViewBuilder extends ViewBuilder<Branch> {
 	
 	private class BranchUI extends ViewTag {
 		TextView branchName;
+		ImageView image;
 	}
 
 	public BranchViewBuilder() {
@@ -28,6 +32,7 @@ public class BranchViewBuilder extends ViewBuilder<Branch> {
 		BranchUI row  = new BranchUI();
 		row.branchName = (TextView) view.findViewById(R.id.opBranchName);
 		row.branchName.setMovementMethod(LinkMovementMethod.getInstance());
+		row.image = (ImageView) view.findViewById(R.id.branchImage);
 		return row;
 	}
 
@@ -36,6 +41,13 @@ public class BranchViewBuilder extends ViewBuilder<Branch> {
 		BranchUI row = (BranchUI) view.getTag();
 		row.branchName.setText(branch.getName());
 		viewAdapter.setToViewHtml(branch, row.branchName, FIELD_NAME);
+		row.image.setTag("http://totallystockholm.se/wp-content/uploads/2014/02/kfc.jpg");
+		Drawable dr = ImageLoaderTask.getInstance().loadImage(viewAdapter, row.image);
+		if (dr != null) {
+			row.image.setImageDrawable(dr);
+		} else {
+			row.image.setImageResource(R.drawable.lotteria_logo);
+		}
 	}
 
 }
